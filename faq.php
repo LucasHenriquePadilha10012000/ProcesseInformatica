33ac28cf1baa530ba009f46a6451f7d086b728be
lucas_h_padilha <lucas_h_padilha@SNRSL1ELBIET056>
2016-11-16 09:53:33 -0200

    primeiro arquivo

---
+++ faq.php
@@ -0,0 +1,80 @@
+<div class="titulo">
+    <h2>Opções de lanches</h2>
+</div>
+<hr>
+<div class="promocao">
+    <div class="img">
+        <a href="#">
+            <img src="img/mis.jpg" width="300" height="200">
+        </a>
+        <div class="desc">
+            Integral <br>
+            R$ 4,99
+        </div>
+    </div>
+    <div class="img">
+        <a href="#">
+            <img src="img/batata.jpg" width="300" height="200">
+        </a>
+        <div class="desc">
+            Batata-Frita <br>
+            R$ 3,99
+        </div>
+    </div>
+    <div class="img">
+        <a href="#">
+            <img src="img/x.jpg" width="300" height="200">
+        </a>
+        <div class="desc">
+            Double <br>
+            R$ 11,99
+        </div>
+    </div>
+    <div class="img">
+        <a href="#">
+            <img src="img/canudo.jpg" width="300" height="200">
+        </a>
+        <div class="desc">
+            Canudo <br>
+            R$ 15,99
+        </div>
+    </div>
+    <div class="img">
+        <a href="#">
+            <img src="img/po.jpg" width="300" height="200">
+        </a>
+        <div class="desc">
+            Batata-smile <br>
+            R$ 5,99
+        </div>
+    </div>
+    <div class="img">
+        <a href="#">
+            <img src="img/ca.png" width="300" height="200">
+        </a>
+        <div class="desc">
+            Cachorro-Quente <br>
+            R$ 6,99
+        </div>
+    </div>
+    <div class="img">
+        <a href="#">
+            <img src="img/pi.jpg" width="300" height="200">
+        </a>
+        <div class="desc">
+            Pizza <br>
+            R$ 69,99
+        </div>
+    </div>
+    <div class="img">
+        <a href="#">
+            <img src="img/aça.jpg" width="300" height="200">
+        </a>
+        <div class="desc">
+            Açai <br>
+            R$ 7,99
+        </div>
+    </div>
+</div>
+
+
